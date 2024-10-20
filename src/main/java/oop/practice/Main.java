4fd831bad3d4ea65c789f951d2fb960233ed386a
Andrei CEAETCHII <ceaetchiiandrei@gmail.com>
2024-10-20 20:37:58 +0300

    add class classification, and move the logic  of classification in that class

--- src/main/java/oop/practice/Main.java
+++ src/main/java/oop/practice/Main.java
@@ -1,23 +1,14 @@
 package oop.practice;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+
 
 public class Main {
   public static void main(String[] args) throws IOException {
     JsonNode data = readFile.readInput();
     readFile.parseInput(data);
-
-    for ( creature creature: readFile.creatureList){
-      creature.findUniverse();
-      creature.printCreature();
-    }
 
     View.WriteInOutputFile();
 
--- src/main/java/oop/practice/creature.java
+++ src/main/java/oop/practice/creature.java
@@ -1,15 +1,12 @@
 package oop.practice;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 public class creature {
     private int id;
     private Boolean isHumanoid;
     private String planet;
     private Integer age;
     private String[] traits;
-    private String universe;
+    private Classification universe;
 
     public creature(int id, Boolean isHumanoid, String planet, Integer age, String[] traits) {
         this.id = id;
@@ -17,6 +14,7 @@ public class creature {
         this.planet = planet;
         this.age = age;
         this.traits = traits  ;
+        this.universe = new Classification(isHumanoid, planet, age, traits);
     }
 
     public int getId() {
@@ -35,7 +33,7 @@ public class creature {
         return traits;
     }
     public String getUniverse() {
-        return universe;
+        return universe.findUniverse();
     }
 
     public void printCreature() {
@@ -46,90 +44,5 @@ public class creature {
 //        System.out.println("Age: " + age);
 //        System.out.println("Traits: " + Arrays.toString(traits));
         System.out.println("Universe: " + universe);
-    }
-
-    public void findUniverse(){
-        if(checkIfIsFromStarWars()){
-            this.universe = "Star Wars";
-        } else if (checkIfIsFromMarvel()){
-            this.universe = "Marvel";
-        } else if (checkIfIsFromHitchhikers()){
-            this.universe = "Hitchhiker's";
-        } else if (checkIfIsFromLordOfTheRings()){
-            this.universe = "Lord of the rings";
-        }
-        else {
-        this.universe = "Universe not found";}
-    }
-
-    private boolean containsTrait(String trait) {
-        return traits != null && Arrays.asList(traits).contains(trait);
-    }
-
-    private Boolean checkIfIsFromStarWars(){
-        //check if is a valid Kashyyyk
-        boolean validHumanoid = Boolean.FALSE.equals(isHumanoid) || isHumanoid == null;
-        boolean validPlanet = Objects.equals(planet, "Kashyyyk") || planet == null;
-        boolean validAge = age == null || (age >= 0 && age <= 400);
-        boolean validTraits = containsTrait("HAIRY") || containsTrait("TALL") || traits == null;;
-
-        if (validHumanoid && validPlanet && validAge && validTraits){
-            return true;
-        }
-        //check if is a valid ewok
-        validHumanoid = Boolean.FALSE.equals(isHumanoid) || isHumanoid == null;
-        validPlanet = Objects.equals(planet, "Endor") || planet == null;
-        validAge = age == null || (age >= 0 && age <= 60);
-        validTraits = containsTrait("SHORT") || containsTrait("HAIRY") || traits == null;
-
-        return validHumanoid && validPlanet && validAge && validTraits;
-    }
-
-    private Boolean checkIfIsFromMarvel(){
-        //check if is a valid Asgardian
-        boolean validHumanoid = Boolean.TRUE.equals(isHumanoid) || isHumanoid == null;
-        boolean validPlanet = Objects.equals(planet, "Asgard") || planet == null;
-        boolean validAge = age == null || (age >= 0 && age <= 5000);
-        boolean validTraits = containsTrait("BLONDE") || containsTrait("TALL") || traits == null;
-
-        return validHumanoid && validPlanet && validAge && validTraits;
-    }
-
-    private Boolean checkIfIsFromHitchhikers (){
-        //check if is a valid Betelgeusian
-        boolean validHumanoid = Boolean.TRUE.equals(isHumanoid) || isHumanoid == null;
-        boolean validPlanet = Objects.equals(planet, "Betelgeuse") || planet == null;
-        boolean validAge = age == null || (age >= 0 && age <= 100);
-        boolean validTraits = containsTrait("EXTRA_ARMS") || containsTrait("EXTRA_HEAD") || traits == null;//check if is a valid Asgardian
-
-        if (validHumanoid && validPlanet && validAge && validTraits){
-            return true;
-        }
-        //check if is a valid Vogons
-        validHumanoid = Boolean.FALSE.equals(isHumanoid) || isHumanoid == null;
-        validPlanet = Objects.equals(planet, "Vogsphere") || planet == null;
-        validAge = age == null || (age >= 0 && age <= 200);
-        validTraits = containsTrait("GREEN") || containsTrait("BULKY") || traits == null;
-
-        return validHumanoid && validPlanet && validAge && validTraits;
-    }
-
-    private Boolean checkIfIsFromLordOfTheRings (){
-        //check if is a valid Elf
-        boolean validHumanoid = Boolean.TRUE.equals(isHumanoid) || isHumanoid == null;
-        boolean validPlanet = Objects.equals(planet, "Earth") || planet == null;
-        boolean validTraits = containsTrait("BLONDE") || containsTrait("POINTY_EARS") || traits == null;//check if is a valid Asgardian
-
-
-        if (validHumanoid && validPlanet && validTraits){
-            return true;
-        }
-        //check if is a valid Dwarf
-        validHumanoid = Boolean.TRUE.equals(isHumanoid) || isHumanoid == null;
-        validPlanet = Objects.equals(planet, "Earth") || planet == null;
-        boolean validAge = age == null || (age >= 0 && age <= 200);
-        validTraits = containsTrait("SHORT") || containsTrait("BULKY") || traits == null;
-
-        return validHumanoid && validPlanet && validAge && validTraits;
     }
 }
